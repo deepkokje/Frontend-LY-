0e8eed78e8c73be11b2865a7697f85519f003afd
deepkokje <deepkokje@gmail.com>
2020-02-10 10:28:23 +0530

    deployed api

--- Pages/General.js
+++ Pages/General.js
@@ -1,15 +1,59 @@
 import React , {Component} from 'react';
-import {StyleSheet,View,Text} from 'react-native';
+import {StyleSheet,View,Text,FlatList,ActivityIndicator} from 'react-native';
 
 
 export default class General extends Component{
-    render(){
-        return(
-        <View style ={styles.MainConatiner} >
-             <Text style={{ fontSize: 23 }}> General Screen</Text>
-        </View>)
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://newsapi-65f63.firebaseapp.com/fetch-positivenews')
+      .then((response) => response.json())
+      
+      .then((responseJson) => {
+      
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+       
+      });
+  }
+
+  
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
     }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.Title}, 
+          {item.Description}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+    );
+  }
 }
+
+
 const styles = StyleSheet.create({
     MainContainer: {
       flex: 1,
